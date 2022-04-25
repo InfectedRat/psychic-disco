d863db3acf8fa6ac0856683ecf99fe553f65da5e
Maxim Babichev <birdclock@gmail.com>
2022-04-25 21:03:52 +0300

    test function

---
+++ 1.5 stepik.js
@@ -0,0 +1,12 @@
+function testIf(a, b) {
+    var x;
+    if (a>b) {x=a+b;}
+    else {x=a*b;}
+    return x;
+}
+
+function testfucksum(a, b) {
+    return a+b;
+}
+
+console.log(testfucksum(1,3));
