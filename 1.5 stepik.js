272ecf5ad609982b04a859b7b90c4c3689c53c51
Maxim Babichev <birdclock@gmail.com>
2022-04-28 13:14:09 +0300

    switch test

--- 1.5 stepik.js
+++ 1.5 stepik.js
@@ -17,6 +17,44 @@ function testEifElse(a,b) {
     return x;
 }
 
+function testCase(a) {
+    var x;
+    var a;
+        switch(a) {
+            case 1:
+                x = 'Один'
+            break;
+            case 2:
+                x = 'Два'
+            break;
+            case 3:
+                x = 'Три'
+            break;
+            case 4:
+                x = 'Четыре'
+            break;
+            case 5:
+                x = 'Пять'
+            break;
+            case 6:
+                x = 'Шесть'
+            break;
+            case 7:
+                x = 'Семь'
+            break;
+            case 8:
+                x = 'Восемь'
+            break;
+            case 9:
+                x = 'Девять'
+            break;
+            case 0:
+                x = 'Ноль'
+            break;
+            default: 
+                x = 'Переменная не равна от 0 до 9 ';
+        }
+    return x;
+}
 
-
-console.log(testEifElse(9,9))
+console.log(testCase(6));
