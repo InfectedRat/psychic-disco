dbac271e2c98afd71fbc4de0d1b366415b84de4a
Maxim Babichev <birdclock@gmail.com>
2022-05-02 03:29:07 +0300

    wheli test

--- 1.6 stepik.js
+++ 1.6 stepik.js
@@ -8,3 +8,18 @@ function testFactorial(a) {
 }
 
 console.log(testFactorial(9));
+
+
+function testWhile(a) {
+    var x=0;
+    var i=0;
+    while (i<=a) {
+        if (i%2==0) {
+            x=x+i
+        }
+    i=i+1;
+    }
+    return x;
+}
+
+console.log(testWhile(5))
