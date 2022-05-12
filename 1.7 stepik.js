676f133c797e79ed1a9b48f66b1120dafc982aaa
Maxim Babichev <birdclock@gmail.com>
2022-05-12 22:33:15 +0300

    1.7 still

--- 1.7 stepik.js
+++ 1.7 stepik.js
@@ -6,3 +6,12 @@ function mySquare(x) {
     if (x <= 1) return 1;        
     return x * factorial(x-1);   
   }
+
+  function mySquare(x) {
+    return x*x;
+  }
+
+  function factorial(x) {        
+    if (x <= 1) return 1;        
+    return x * factorial(x-1);   
+  }
