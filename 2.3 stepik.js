973e2e54d6d6b5e1318eb4d11b7419efe47a67c1
Maxim Babichev <birdclock@gmail.com>
2022-05-23 21:43:52 +0300

    2.3 still

--- 2.3 stepik.js
+++ 2.3 stepik.js
@@ -8,3 +8,14 @@ testStr('sss','sss');
 
 var cat = "Cat";
 console.log(cat.length); 
+
+function testStr(a, b) {
+    //var c = a;
+    //var d = b;
+    return(a.length+b.length);
+}
+
+testStr('sss','fgfdgdfgdfg');
+
+var cat = "Cat";
+console.log(cat.length); 
